a595d91936db7b58d77e0bcd1a06669728f40626
rohto4 <rohto111117@gmail.com>
2020-04-21 14:37:39 +0900

    aaa

--- ScheduleTest.py
+++ ScheduleTest.py
@@ -34,8 +34,6 @@ class ScheduleTest:
     def readtweetschedule():
         # search param setting
         screen_name = "arai_rehabili"
-
-
         params = {
             'q':screen_name,
             'lang':'ja',
@@ -43,12 +41,13 @@ class ScheduleTest:
             'count':10
             }
 
-    def main():
+    # credentials.json または token.pickle を使用し、
+    # GoogleCalendar を取得する
+    def readCalendar():
+        '''
+        /弄らない
+        '''
         creds = None
-
-        '''
-        弄らない
-        '''
         # token.pickle ファイルは認証フローの初回完了時に作成される
         # access token & refresh tokemnを保持する
         if os.path.exists('token.pickle'):
@@ -69,20 +68,21 @@ class ScheduleTest:
                 pickle.dump(creds, token)
 
         # 対象のカレンダーを取得
-        service = build('calendar', 'v3', credentials=creds)
-        ScheduleTest.readschedule()
-        print(ScheduleTest.readschedule())
-        '''
-        /弄らない
-        '''
+        return build('calendar', 'v3', credentials=creds)
+
+
+    def main():
+        # credentials.json または token.pickle を使用し、
+        # GoogleCalendar を取得する
+        calendar = ScheduleTest.readCalendar()
 
         ### 挿入用テキスト処理 ###
 
         # 日付取り出し
         yearmon = ScheduleTest.readschedule()[0]
-        tmp = yearmon.split(".")
-        year = int(tmp[0])
-        mon = int(tmp[1])
+        tmp_ym = yearmon.split(".")
+        year = int(tmp_ym[0])
+        mon = int(tmp_ym[1])
 
         # チェック用比較用カレンダー設定
         if mon == 1 or mon == 3 or mon == 5 or mon == 7 or mon == 8 or mon == 10 or mon == 12:
@@ -93,46 +93,38 @@ class ScheduleTest:
             num_days = 30
 
         # 内容部
-        for i in ScheduleTest.readschedule():
+        for txt in ScheduleTest.readschedule():
             # 一行を分割
-            txtl = i.split('_')
+            line_txt = txt.split('_')
             # 改行のみの行をスキップ
-            if(len(txtl) == 1): continue
+            if(len(line_txt) == 1): continue
             # コメント行をスキップ
             if("#" is i[0]): continue
 
             # start date, end date設定
             year_s = year_e = year
             mon_s = mon_e = mon
-            day_s = day_e = int(txtl[0])
+            day_s = day_e = int(line_txt[0])
 
             # 変動する挿入テキストの設定
             # Hour Minutu
             # [day text]
-            if(len(txtl) == 2):
-                ins = {'hour_s', 0}
-                ins = {'min_s', 0}
-                ins = {'hour_e', 0}
-                ins = {'min_e', 0}
+            if(len(line_txt) == 2):
+                ins = {'hour_s': 0, 'min_s': 0, 'hour_e': 0, 'min_e': 0}
                 txt_i = 1
             # [day time text]
-            elif(len(txtl[1]) == 4):
-                ins = {'hour_s', int(txtl[1][0:2])}
-                ins = {'min_s', int(txtl[1][2:4])}
-                ins = {'hour_e', int(txtl[1][0:2])}
-                ins = {'min_e', int(txtl[1][2:4])}
+            elif(len(line_txt[1]) == 4):
+                ins = {'hour_s': int(line_txt[1][0:2]), 'min_s': int(line_txt[1][2:4]),
+                       'hour_e': int(line_txt[1][0:2]), 'min_e': int(line_txt[1][2:4])}
                 txt_i = 2
             # [day time-time text]
-            elif(len(txtl[1]) == 9):
-                ins = {'hour_s', int(txtl[1][0:2])}
-                ins = {'min_s', int(txtl[1][2:4])}
-                ins = {'hour_e', int(txtl[1][5:7])}
-                ins = {'min_e', int(txtl[1][7:9])}
+            elif(len(line_txt[1]) == 9):
+                ins = {'hour_s': int(line_txt[1][0:2]), 'min_s': int(line_txt[1][2:4]),
+                       'hour_e': int(line_txt[1][5:7]), 'min_e': int(line_txt[1][7:9])}
                 txt_i = 2
-
-            # text
-            ins = {'txt', txtl[txt_i]}
-            print(ins)
+            ins['txt'] = line_txt[txt_i]
+            # check
+            # print(ins)
 
             # 12月の処理
             if(mon == 12 and day_e == 31):
@@ -145,35 +137,34 @@ class ScheduleTest:
 
             # 挿入データ設定
             event = {
-                'summary': '{}'.format(ins['txt']),
+                'summary': '{}'.format(ins.get('txt')),
                 'location': 'aliesan\'s nest',
-                'description': '{}'.format(),
+                'description': '',
                 'start': {
                     'dateTime': '{0}-{1:02}-{2}T{3:02}:{4:02}:{5:02}'
-                    .format(year_s, mon_s, day_s, ins['hour_s'], ins['min_s'], 0),
+                    .format(year_s, mon_s, day_s, ins.get('hour_s'), ins.get('min_s'), 0),
                     'timeZone': 'Japan',
                 },
                 'end': {
                     'dateTime': '{0}-{1:02}-{2}T{3:02}:{4:02}:{5:02}'
-                    .format(year_e, mon_e, day_e, ins['hour_e'], ins['min_e'], 0),
+                    .format(year_e, mon_e, day_e, ins.get('hour_e'), ins.get('min_e'), 0),
                     'timeZone': 'Japan',
                 },
             }
-            print(event)
+            # check
+            # print(event)
 
             # 挿入
-            event = service.events().insert(calendarId='arai.rehabilitation@gmail.com', body=event).execute()
-            print(event['id'])
-
+            event = calendar.events().insert(calendarId='arai.rehabilitation@gmail.com', body=event).execute()
             # 挿入値をクリア
             ins.clear()
         # /内容部
 
     # twitterセッション取得
     def createSession():
-        session = OAuth1Session(TCK, TCS, TAK, TAS)
         # session = OAuth1Session(os.environ['CONSUMER_KEY'],os.environ['CONSUMER_SECRET'],\
         #                         os.environ['ACCESS_KEY'], os.environ['ACCESS_TOKEN_SECRET'])
+        session = OAuth1Session(TCK, TCS, TAK, TAS)
         return session
 
 if __name__ == '__main__':
